9a02b635ae22dd064240837b04f1039c38908e76
wdsoft <jiwendong09@163.com>
2018-09-10 22:49:07 +0800

    add problem 92

--- src/main/java/com/github/wdsoft/linkedlist/ReverseLinkedList_92.java
+++ src/main/java/com/github/wdsoft/linkedlist/ReverseLinkedList_92.java
@@ -45,18 +45,4 @@ public class ReverseLinkedList_92 {
 
         return end;
     }
-
-    public static void main(String[] args) {
-        ReverseLinkedList_92 obj = new ReverseLinkedList_92();
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        obj.reverseBetween(node1, 2, 4);
-    }
 }
