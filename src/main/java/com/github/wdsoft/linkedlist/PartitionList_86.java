a712193a00bdf0b466ccef0cd3dafb2d369ac540
wdsoft <jiwendong09@163.com>
2018-09-11 21:03:17 +0800

    add problem 86

--- src/main/java/com/github/wdsoft/linkedlist/PartitionList_86.java
+++ src/main/java/com/github/wdsoft/linkedlist/PartitionList_86.java
@@ -26,20 +26,4 @@ public class PartitionList_86 {
 
         return leftHead.next;
     }
-
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(4);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(2);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(2);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        PartitionList_86 obj = new PartitionList_86();
-        obj.partition(node1, 3);
-    }
 }
